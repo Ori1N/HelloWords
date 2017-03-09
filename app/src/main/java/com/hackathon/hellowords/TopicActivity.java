36d16e548b2585755a4e4180ce35e503b61697f1
ticatac <lior.keshet@gmail.com>
2017-03-09 21:43:12 +0200

    added topic xml

--- app/src/main/java/com/hackathon/hellowords/TopicActivity.java
+++ app/src/main/java/com/hackathon/hellowords/TopicActivity.java
@@ -1,11 +1,12 @@
 package com.hackathon.hellowords;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TopicActivity extends AppCompatActivity {
 
@@ -15,6 +16,20 @@ public class TopicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_topic);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        ImageView topicImage1 = (ImageView) findViewById(R.id.IV_image1);
+        topicImage1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), this.toString()+"msg msg", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
+
+
     }
 
 }
