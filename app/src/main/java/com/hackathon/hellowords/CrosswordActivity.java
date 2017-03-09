9c920bd8152fe24afec1394f1b4c1f12d7b5b75c
Ori <ori.n.1621@gmail.com>
2017-03-10 00:50:59 +0200

    added basic CrosswordProvider

--- app/src/main/java/com/hackathon/hellowords/CrosswordActivity.java
+++ app/src/main/java/com/hackathon/hellowords/CrosswordActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.hackathon.hellowords.data.CrosswordProvider;
+import com.hackathon.hellowords.model.CrosswordObject;
+
 public class CrosswordActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +15,12 @@ public class CrosswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_crossword);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initializeUiFromCrossword(CrosswordProvider.getCrossword());
+    }
+
+    private void initializeUiFromCrossword(CrosswordObject obj) {
+        
     }
 
 }
---
+++ app/src/main/java/com/hackathon/hellowords/data/CrosswordProvider.java
@@ -0,0 +1,20 @@
+package com.hackathon.hellowords.data;
+
+
+/**
+ * Created by Ori on 09/03/2017.
+ */
+
+public class CrosswordProvider {
+
+    public static char[][] getCrossword() {
+        return new char[][] {
+                { 0,    0,  'T',    0,      0,      0,      0,      0,      0,      0 },
+                { 0,    0,  'I',    0,      0,      0,      0,      'B',    0,      'C' },
+                { 'D', 'O', 'G',    0,      0,      0,      0,      'E',    0,      'A' },
+                { 0,    0,  'E',    'L',    'E',    'P',    'H',    'A',    'N',    'T' },
+                { 0,    0,  'R',    0,      0,      0,      0,      'R',    0,      0 }
+        };
+
+    }
+}
