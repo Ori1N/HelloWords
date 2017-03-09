993f7a803aa29d92ccd59879d61bab00f45b1dda
Ori <ori.n.1621@gmail.com>
2017-03-09 22:11:26 +0200

    basic crossword screen

--- app/src/main/java/com/hackathon/hellowords/SplashScreen.java
+++ app/src/main/java/com/hackathon/hellowords/SplashScreen.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
-    private static final boolean GO_TO_CROSSWORD = false;
+    private static final boolean GO_TO_CROSSWORD = true;
 
 
     @Override
